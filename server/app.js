623e28382d35a392624ec00ba6851c5bd499387e
songzhongkun <zhongkun.song@vcg.com>
2018-04-26 17:08:28 +0800

    fix

--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@ var app = express();
 //方法1：通过express.static访问静态文件，这里访问的是ajax.html
 // app.use(express.static("./stone.html"));
 //方法2：使用fs.readFile打开html文件
-app.get('/stone.html', function (request, response) {
-  fs.readFile("./" + request.path.substr(1), function (err, data) {
+app.get('/', function (request, response) {
+  fs.readFile('./stone.html' + request.path.substr(1), function (err, data) {
     if (err) {
       console.log(err); //404：NOT FOUND
       response.writeHead(404, {"Content-Type": "text/html"});
@@ -18,5 +18,5 @@ app.get('/stone.html', function (request, response) {
 });
 app.listen(4001, function () {
   //监听http://127.0.0.1:3000端口
-  console.log("server start");
+  console.log("server start 4001");
 });
