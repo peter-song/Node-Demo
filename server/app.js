93829b099243d592a470e74e22652a39bee5e7ed
songzhongkun <zhongkun.song@vcg.com>
2018-04-26 17:02:04 +0800

    delete file

--- server/app.js
+++ server/app.js
@@ -1,7 +1,22 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1>Node.js</h1>');
-  res.end('<p>Hello World</p>');
-}).listen(4001);
-console.log("HTTP server is listening at port 4001.");
+var express = require('express');
+var fs = require("fs");
+var app = express();
+//方法1：通过express.static访问静态文件，这里访问的是ajax.html
+// app.use(express.static("./stone.html"));
+//方法2：使用fs.readFile打开html文件
+app.get('/stone.html', function (request, response) {
+  fs.readFile("./" + request.path.substr(1), function (err, data) {
+    if (err) {
+      console.log(err); //404：NOT FOUND
+      response.writeHead(404, {"Content-Type": "text/html"});
+    } else {
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write(data.toString());
+    }
+    response.end();
+  });
+});
+app.listen(4001, function () {
+  //监听http://127.0.0.1:3000端口
+  console.log("server start");
+});
